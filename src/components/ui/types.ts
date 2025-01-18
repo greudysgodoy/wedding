122823189fe3aaea76f72125db9e2db5bfd2bc5b
Greudys Godoy <greudysgodoy@gmail.com>
2025-01-18 12:18:38 -0400

    feat: home - added count down - added address - added date

--- src/components/ui/types.ts
+++ src/components/ui/types.ts
@@ -1,9 +1,9 @@
-export interface Image {
-  src: string; // URL de la imagen
+type ImageSource = string | ImageMetadata;
+export interface ImageProps {
+  src: ImageSource; // URL de la imagen
   alt: string; // Descripción de la imagen
   caption: string; // Texto para mostrar sobre la imagen
+  width?: number; // Ancho de la imagen
+  height?: number; // Alto de la imagen
 }
 
-export interface Props {
-  images: Image[]; // Lista de imágenes para el carrusel
-}
